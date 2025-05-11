15520bed2e1e4d2a8f54aba602924fda88f89652
kristian.hernandez@encora.com <kristian.hernandez@ENCEDCREMLT0781.local>
2025-05-11 16:23:45 -0600

    Redesigned all my solution

--- Python/number_of_unequal_triplets_in_array.py
+++ Python/number_of_unequal_triplets_in_array.py
@@ -1,36 +1,23 @@
 class Solution:
     def unequalTriplets(self, nums: List[int]) -> int:
-        x, y = 0, 1
-        x_counter, y_counter = 0, 1
-        array = [nums[x]]
+        i, j, k = 0, 0, 0 # These are my pointers
+        len_nums = len(nums)
         triplets = 0
 
-        for n in nums:
+        # i loop
+        while i < len_nums - 2:
 
-            # This line restarts the array and add the value of nums[x] to it
-            array = [nums[x]]
+            j = i + 1
+            # j loop
+            while j < len_nums - 1:
 
-            while True:
-                
-                # This line validates that y have a valid number to avoid an Error
-                if y >= len(nums):
-                    break
-
-                # This block compares values, adds them to the array and check for triplets completion.
-                if nums[x] != nums[y]:
-
-                    if nums[y] not in array:
-                        array.append(nums[y])
-
-                    if len(array) == 3:
+                k = j + 1
+                # k loop
+                while k < len_nums:
+                    if nums[i] != nums[j] and nums[i] != nums[k] and nums[j] != nums[k]:
                         triplets += 1
-                        break
+                    k += 1
+                j += 1
+            i += 1
 
-                # This line increments the value of y
-                y += 1
-            # This lines increments x and y counters and restart x and y
-            x_counter += 1
-            y_counter += 1
-            x, y = x_counter, y_counter
-        
         return triplets
